3acb772fea59eb9792351b3d3233529dcf8fa98d
WouterDX <wouter.dierckx@gmail.com>
2018-04-29 23:36:14 +0200

    added search

---
+++ content/posts/fourth_post.md
@@ -0,0 +1,19 @@
+Title: First Post - Part 4
+Date: 2018-04-29 22:01
+Modified: 2018-04-29 22:01
+Status: published
+Category: example posts
+Tags: python, pelican, blog, tables
+Slug: fourth-post
+Authors: Wouter Dierckx
+Series: example-series
+Series_index: 4
+Summary: What, even more to add?
+
+
+Demonstration of tables.
+
+| A markdown table | with sauce | 
+| --- | --- | 
+| should not | be spared |
+| without | sufficient appetite | 
--- pelicanconf.py
+++ pelicanconf.py
@@ -12,10 +12,23 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = 'en' #crashes if not in translation and diffrent fromt I18N settings.
 
+IGNORE_FILES = ['.ipynb'] #does not help
+
 # Pelican theme setup
 THEME = 'pelican-themes/pelican-bootstrap3'
 BOOTSTRAP_THEME = 'flatly'
 #THEME = 'greizgh/pelican-material' #needs more plugins first
+
+# JAVASCRIPT 
+CUSTOM_CSS = 'static/css/custom.css'
+CUSTOM_JS = 'static/js/custom.js'
+
+STATIC_PATHS = ['extra']
+EXTRA_PATH_METADATA = {
+    'extra/custom.css': {'path': 'static/css/custom.css'},
+	'extra/custom.js': {'path': 'static/css/custom.js'}
+	}
+	
 
 # PLUGINS
 JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
@@ -28,16 +41,15 @@ PLUGINS = [
     'liquid_tags.notebook',
     'liquid_tags.include_code',
     'render_math',
-    'pelican-ipynb.markup' 
-	]
+    'pelican-ipynb.markup',
+    'tipue_search']
 
 # I18N CONFIG
 #I18N_TEMPLATES_LANG = 'nl' #dutch language code
 I18N_TEMPLATES_LANG = 'en' #same as default language code
 
-# Jupyter markup config
-IGNORE_FILES = ['.ipynb']
-
+# TIPUE SEARCH CONFIG
+DIRECT_TEMPLATES = ('index','tags', 'categories', 'authors', 'archives', 'search')
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
